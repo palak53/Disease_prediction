5b9ac041f4cd196d8facbc4ae75d6de21be957d4
palak53 <66553278+palak53@users.noreply.github.com>
2021-07-02 23:32:27 +0530

    first commit

---
+++ prediction_model/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+# admin.site.register(patient)
+# admin.site.register(doctor)
+# admin.site.register(diseaseinfo)
+# admin.site.register(consultation)
+# admin.site.register(rating_review)
